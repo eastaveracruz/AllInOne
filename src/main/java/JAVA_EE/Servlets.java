de4b9b7716688c5a8696a8b7782672a183dbb590
Justaway <eastaveracruz@gmail.com>
2018-06-04 09:21:22 +0300

    Hello world - 1.1

--- src/main/java/JAVA_EE/Servlets.java
+++ src/main/java/JAVA_EE/Servlets.java
@@ -1,7 +1,17 @@
 package JAVA_EE;
 
-public class Servlets {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlets extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hello World");
     }
 }
